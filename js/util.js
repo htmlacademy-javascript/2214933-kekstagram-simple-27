541f90e9db105cc6c835c8928c976fc07827333c
Orehnik <Yorik94@mail.ru>
2022-10-17 19:14:17 +0300

    сделал 5 задание в 4 ветке

---
+++ js/util.js
@@ -0,0 +1,23 @@
+// случайное число из диапозона
+function getRandomInt(min, max) {
+  if (min < 0 || max < 0) {
+    return NaN;
+  }
+  if (min > max) {
+    return getRandomInt(max, min);
+  }
+  if (min === max) {
+    return max;
+  }
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+// проверка длины коментария
+const compareLengths = (string, maxLength) => string.length <= maxLength;
+
+// console.log(compareLengths('asdsad', 3));
+
+//случайный элемент масива
+const getRandomArrayElement = (elements) => elements[getRandomInt(0, elements.length - 1)];
+
+export {getRandomArrayElement, getRandomInt, compareLengths};
